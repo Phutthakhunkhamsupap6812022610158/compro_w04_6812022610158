d0b26db498fcecfb31de9e0c07a5f5332923a57c
Phutthakhunkhamsupap6812022610158 <s6812022610158@email.kmutnb.ac.th>
2025-07-17 18:32:02 +0700

    Delete coding_w04-01.c

--- coding_w04-01.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>.         // include คือ Keyword ในการนำคำสั่งนำเข้า Libraya
-
-int main() {               // 2. int คือ Keyword , mai คือ Identifier ,() คือ Operator
-    printf("Hello,\n");    // 2.1 printf คือ Identifier , Hello,/n คือ Constant , ; คือ Operator
-    printf("C Token\n");   // 2.2 printf คือ Identifier , C Token/n คือ Constant , ; คือ Operator
-
-    return 0;              // 3. return คือ Keyword , 0 คือ Constant , ; คือ Operator
-}
